378c5fc490249e55a2af83c60cb7c1f9146130e7
Vinicius Andrade <ozirispc@gmail.com>
2023-05-28 17:25:02 -0400

    first commit

--- src/app/pages/courses/course-list/course-list.component.ts
+++ src/app/pages/courses/course-list/course-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-
+  public courseList = [...Array(15).keys()];
   constructor() { }
 
   ngOnInit() {
